7201a4ab4b735fe77dd101e0e6a2045e7b8ccc81
Alperen Gönül <alperen2009g@gmail.com>
2026-07-01 16:20:48 +0300

    finish project

--- TepeGoz/TepeGoz/Form1.Designer.cs
+++ TepeGoz/TepeGoz/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.hesapAdiTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bulBtn = new System.Windows.Forms.Button();
+            this.sonuclarLB = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bagisBtn = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // hesapAdiTxt
+            // 
+            this.hesapAdiTxt.Location = new System.Drawing.Point(85, 10);
+            this.hesapAdiTxt.Name = "hesapAdiTxt";
+            this.hesapAdiTxt.Size = new System.Drawing.Size(446, 20);
+            this.hesapAdiTxt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // bulBtn
+            // 
+            this.bulBtn.Location = new System.Drawing.Point(537, 8);
+            this.bulBtn.Name = "bulBtn";
+            this.bulBtn.Size = new System.Drawing.Size(75, 23);
+            this.bulBtn.TabIndex = 2;
+            this.bulBtn.Text = "Bul";
+            this.bulBtn.UseVisualStyleBackColor = true;
+            this.bulBtn.Click += new System.EventHandler(this.bulBtn_Click);
+            // 
+            // sonuclarLB
+            // 
+            this.sonuclarLB.FormattingEnabled = true;
+            this.sonuclarLB.Location = new System.Drawing.Point(15, 45);
+            this.sonuclarLB.Name = "sonuclarLB";
+            this.sonuclarLB.Size = new System.Drawing.Size(597, 342);
+            this.sonuclarLB.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(411, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Alperen Buba v1";
+            // 
+            // bagisBtn
+            // 
+            this.bagisBtn.BackColor = System.Drawing.Color.Lime;
+            this.bagisBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bagisBtn.Location = new System.Drawing.Point(331, 401);
+            this.bagisBtn.Name = "bagisBtn";
+            this.bagisBtn.Size = new System.Drawing.Size(75, 23);
+            this.bagisBtn.TabIndex = 5;
+            this.bagisBtn.Text = "Bağış Yap";
+            this.bagisBtn.UseVisualStyleBackColor = false;
+            this.bagisBtn.Click += new System.EventHandler(this.bagisBtn_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 401);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(310, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.bagisBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sonuclarLB);
+            this.Controls.Add(this.bulBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.hesapAdiTxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TepeGoz | OSINT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox hesapAdiTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bulBtn;
+        private System.Windows.Forms.ListBox sonuclarLB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bagisBtn;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- TepeGoz/TepeGoz/Form1.cs
+++ TepeGoz/TepeGoz/Form1.cs
@@ -1,20 +1,163 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
 
 namespace TepeGoz
 {
     public partial class Form1 : Form
     {
+        string filePath = "sites.json";
+
+        // ListBox'ta hem isim hem link tutmak için yardımcı sınıf
+        public class SiteItem
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+
+            // ListBox'ta görünen metni burada formatlıyoruz
+            public override string ToString()
+            {
+                return $"{Name} |  {Url}";
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            CheckAndCreateJson();
+
+            sonuclarLB.DoubleClick += sonuclarLB_DoubleClick;
+        }
+
+        private void CheckAndCreateJson()
+        {
+            if (!File.Exists(filePath))
+            {
+                string defaultData = "Instagram|https://www.instagram.com/{user}/\n" +
+                     "GitHub|https://github.com/{user}\n" +
+                     "Twitter|https://twitter.com/{user}\n" +
+                     "YouTube|https://www.youtube.com/@{user}\n" +
+                     "TikTok|https://www.tiktok.com/@{user}\n" +
+                     "Twitch|https://www.twitch.tv/{user}\n" +
+                     "Reddit|https://www.reddit.com/user/{user}";
+                File.WriteAllText(filePath, defaultData);
+            }
+        }
+
+        private async void bulBtn_Click(object sender, EventArgs e)
+        {
+            // 1. İnternet Kontrolü
+            if (!NetworkInterface.GetIsNetworkAvailable())
+            {
+                MessageBox.Show("İnternet Bağlantısı Yok! Lütfen bağlantını kontrol et.");
+                return;
+            }
+
+            string input = hesapAdiTxt.Text.Trim();
+            if (string.IsNullOrEmpty(input)) return;
+
+            sonuclarLB.Items.Clear();
+            bulBtn.Enabled = false;
+            progressBar1.Value = 0;
+
+            // 2. Varyasyonları Hazırla
+            List<string> birlesikVaryasyonlar = new List<string>();
+            List<string> parcaliVaryasyonlar = new List<string>();
+
+            if (input.Contains(" "))
+            {
+                birlesikVaryasyonlar.Add(input.Replace(" ", ""));
+                birlesikVaryasyonlar.Add(input.Replace(" ", "_"));
+                birlesikVaryasyonlar.Add(input.Replace(" ", "."));
+
+                string[] parts = input.Split(' ');
+                parcaliVaryasyonlar.AddRange(parts);
+            }
+            else
+            {
+                birlesikVaryasyonlar.Add(input);
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+            progressBar1.Maximum = (birlesikVaryasyonlar.Count + parcaliVaryasyonlar.Count) * lines.Length;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+
+                // ÖNCE BİRLEŞİK VARYASYONLARI TARA
+                foreach (string username in birlesikVaryasyonlar)
+                {
+                    await TaramaYap(client, username, lines);
+                }
+
+                // EĞER HİÇBİR ŞEY BULAMADIYSA, PARÇALI VARYASYONLARA GEÇ
+                if (sonuclarLB.Items.Count == 0 && parcaliVaryasyonlar.Count > 0)
+                {
+                    sonuclarLB.Items.Add("[*] Birleşik sonuç bulunamadı, parçalı aramaya geçiliyor...");
+                    foreach (string username in parcaliVaryasyonlar)
+                    {
+                        await TaramaYap(client, username, lines);
+                    }
+                }
+            }
+
+            progressBar1.Value = progressBar1.Maximum;
+            bulBtn.Enabled = true;
+            MessageBox.Show("Tarama tamamlandı!");
+        }
+
+        // Tekrardan kurtulmak için tarama metodunu ayırdık
+        private async Task TaramaYap(HttpClient client, string username, string[] lines)
+        {
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                string[] parts = line.Split('|');
+                string url = parts[1].Replace("{user}", username);
+
+                try
+                {
+                    var response = await client.GetAsync(url);
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        sonuclarLB.Items.Add(new SiteItem { Name = $"[+] {parts[0]} ({username})", Url = url });
+                    }
+                }
+                catch { }
+
+                // Burayı değiştirdik: Value'yu manuel artırmak yerine
+                // Maksimum değerin içerisindeyken güvenli şekilde artırıyoruz
+                if (progressBar1.Value < progressBar1.Maximum)
+                {
+                    progressBar1.Value++;
+                }
+            }
+        }
+
+        // Çift tıklama olayı
+        private void sonuclarLB_DoubleClick(object sender, EventArgs e)
+        {
+            if (sonuclarLB.SelectedItem is SiteItem selectedSite)
+            {
+                Process.Start(selectedSite.Url); // Linki tarayıcıda aç
+            }
+        }
+
+        private void bagisBtn_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/AlperenBuba");
+        }
+
+        private void bagisBtn_Click_1(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/AlperenBuba");
         }
     }
 }
